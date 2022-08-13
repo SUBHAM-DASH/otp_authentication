9974acdfb029f0cf9faa730bc9d42ab33dc55ee1
subhamdash <dashsubham095@gmail.com>
2022-08-13 12:59:11 +0530

    otp_authentication has done

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3000;
-const client = require("twilio")(
-  "ACc6d72d0b17c829892e508f281e1318a6",
-  "60c3dc1c4b80dc8e304a08027a0fef2e"
-);
-// 'MGea578267d20b43c9ed3cd2a3a11e8f07'
-app.get("/login", async(req, res) => {
-  client
-  .verify
-  .services('MGea578267d20b43c9ed3cd2a3a11e8f07')
-  .verifications
-  .create({
-    to: 7978904202,
-    channel: "sms"
-  })
-  .then((data)=>{
-    res.status(200).send("successful")
-  });
+const port = 4000;
+const sId = "ACc6d72d0b17c829892e508f281e1318a6";
+const authId = "60c3dc1c4b80dc8e304a08027a0fef2e";
+const phoneNumber = "+14194844328";
+
+const twilio = require('twilio')(sId,authId)
+
+twilio.messages.create({
+  from : `${phoneNumber}`,
+  to : "+91  7978904202",
+  body : "This is finally done"
+})
+.then((res) =>{
+  console.log("message sent");
+})
+.catch((error) =>{
+  console.log(error.messsage);
 });
+
 
 app.listen(port, () => {
   console.log(`server running in ${port}`);
